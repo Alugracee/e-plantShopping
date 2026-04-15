6d0ecad7f96a63e529611c78a68722a12a5fd69b
Alu Grace <alugrace366@gmail.com>
2026-04-15 04:40:06 -0700

    initial commit

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -11,7 +11,7 @@ export const CartSlice = createSlice({
       const existingItem = state.items.find(item => item.name === name)
       if (existingItem){
         // If item already exists in the cart, increase its quantity
-        existingItem.quantity++;
+        existingItem.quantity++; // incrementing the number of the items added to the cart
       } else {
         // If item does not exist, add it to the cart with quantity 1
         state.items.push({ name, image, cost, quantity: 1 });
@@ -19,15 +19,20 @@ export const CartSlice = createSlice({
       }
     },
     removeItem: (state, action) => {
-      // state.items = state.items.filter(item => item.name !== action.payload);
-      const {name, image, cost } = action.payload;
-      const existingItem = state.items.find(item => item.name === name)
-      if (existingItem && existingItem.quantity> 0){
-        existingItem.quantity--;
-      }
+      state.items = state.items.filter(item => item.name !== action.payload);
+      // const {name, image, cost } = action.payload;
+      // const existingItem = state.items.find(item => item.name === name)
+      // if (existingItem && existingItem.quantity> 0){
+      //   existingItem.quantity--;
+      // }
     },
     updateQuantity: (state, action) => {
-
+      const {name, image, quantity} = action.payload;
+      const itemUpTodate = state.items.find(item => item.name === name)
+      if (itemUpTodate){
+        // If item already exists in the cart, increase its quantity
+        itemUpTodate.quantity = quantity; // update the quantity of your item if found rather than incre
+      }
     
     },
   },
